247f9e4af4e09dfdedfaf7219393f64e51fd0057
Sonu B <145313504+sonu-drip@users.noreply.github.com>
2025-09-15 16:14:54 +0530

    Refactor Home component to include User connection logic and improve user experience

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,47 @@
 import { useMiniKit } from "@coinbase/onchainkit/minikit";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useAccount, useConnect } from "wagmi";
+
+const User = () => {
+  const { isConnected, address } = useAccount();
+  const [loading, setLoading] = useState(false);
+  const { connect, connectors } = useConnect({
+    mutation: {
+      onSuccess: () => {
+        setLoading(false);
+      },
+    },
+  });
+  useEffect(() => {
+    if (isConnected) setLoading(false);
+    if (!isConnected) {
+      connect({ connector: connectors[0] });
+    }
+  }, [connect, connectors, isConnected]);
+
+  if (isConnected) {
+    return <div>Connected as {address}</div>;
+  }
+
+  return (
+    <div>
+      {connectors.map((connector) => (
+        <button
+          disabled={!connector.ready || loading}
+          key={connector.id}
+          onClick={() => {
+            setLoading(true);
+            connect({ connector });
+          }}
+        >
+          {connector.name}
+          {!connector.ready && " (unsupported)"}
+          {loading && "..."}
+        </button>
+      ))}
+    </div>
+  );
+};
 
 const Home: React.FC = () => {
   const { setFrameReady, isFrameReady } = useMiniKit();
@@ -8,7 +50,7 @@ const Home: React.FC = () => {
     if (!isFrameReady) setFrameReady();
   }, [isFrameReady, setFrameReady]);
 
-  return <div>Mini App is working Correctly</div>;
+  return <User />;
 };
 
 export default Home;
